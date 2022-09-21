7b9deb40b67eb9e73f4db513b8bc25811da350fd
Temich93s <nil>
2022-09-21 20:57:52 +1000

    init

---
+++ UIKitDZ/UIKitDZ/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  UIKitDZ
+//
+//  Created by 2lup on 21.09.2022.
+//
+
+import UIKit
+
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) { }
+}
---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by 2lup on 21.09.2022.
+//
+
+import UIKit
+
+/// ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
