73a763760880bdb8168f9c9e1e2efe5e20bb29dc
Temich93s <nil>
2022-09-24 14:27:15 +1000

    Исправил все замечания

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-/// ViewController
-class ViewController: UIViewController {
+/// StartViewController - стартовый ViewController
+class StartViewController: UIViewController {
 
-    var labelFIO: UILabel = {
+    var fioLabel: UILabel = {
         let label = UILabel()
         label.frame = CGRect(x: 100, y: 200, width: 200, height: 80)
         label.backgroundColor = UIColor.orange
@@ -18,97 +18,115 @@ class ViewController: UIViewController {
         label.text = "ФИО"
         return label
     }()
-    var labelResultGuessNumber: UILabel = {
+    var resultGuessNumberLabel: UILabel = {
         let label = UILabel()
         label.frame = CGRect(x: 100, y: 300, width: 200, height: 80)
-        label.backgroundColor = UIColor.orange
+        label.backgroundColor = .orange
         label.textAlignment = .center
         label.text = "Сыграем?"
         return label
     }()
-    lazy var buttonGuessNumber: UIButton = {
+    lazy var guessNumberButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 100, y: 400, width: 200, height: 80)
-        button.backgroundColor = UIColor.orange
+        button.backgroundColor = .orange
         button.setTitle("Угадай число", for: .normal)
-        button.addTarget(self, action: #selector(actionButtonGuessNumber), for: .touchUpInside)
+        button.addTarget(self, action: #selector(buttonGuessNumberAction), for: .touchUpInside)
         return button
     }()
-    var labelSum: UILabel = {
+    var sumLabel: UILabel = {
         let label = UILabel()
         label.frame = CGRect(x: 100, y: 500, width: 200, height: 80)
-        label.backgroundColor = UIColor.orange
+        label.backgroundColor = .orange
         label.textAlignment = .center
         label.text = "Результат суммы"
         return label
     }()
-    lazy var buttonSum: UIButton = {
+    lazy var sumButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 100, y: 600, width: 200, height: 80)
-        button.backgroundColor = UIColor.orange
+        button.backgroundColor = .orange
         button.setTitle("Сложить числа", for: .normal)
-        button.addTarget(self, action: #selector(actionButtonSum), for: .touchUpInside)
+        button.addTarget(self, action: #selector(buttonSumAction), for: .touchUpInside)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(labelFIO)
-        self.view.addSubview(labelResultGuessNumber)
-        self.view.addSubview(buttonGuessNumber)
-        self.view.addSubview(labelSum)
-        self.view.addSubview(buttonSum)
+        setUI()
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        let alertController = UIAlertController(title: "ФИО", message: "Введите ФИО", preferredStyle: .alert)
-        let action = UIAlertAction(title: "ОК", style: .default) { _ in
-            if let safeText = alertController.textFields?.first?.text {
-                self.labelFIO.text = safeText
-            }
-        }
-        alertController.addTextField(configurationHandler: nil)
-        alertController.addAction(action)
-        self.present(alertController, animated: true, completion: nil)
+        createStartAlert()
     }
     
-    @objc func actionButtonGuessNumber() {
+    @objc func buttonGuessNumberAction() {
         let alertController = UIAlertController(
             title: "Угадайка",
             message: "Я загадал число от 1 до 3, угадай что это за число?",
             preferredStyle: .alert
         )
-        let action = UIAlertAction(title: "Проверить ответ!", style: .default) { _ in
-            guard let safeText = alertController.textFields?.first?.text else { return }
-            guard let userNumber = Int(safeText) else { return }
+        let alertAction = UIAlertAction(title: "Проверить ответ!", style: .default) { _ in
+            guard
+               let safeText = alertController.textFields?.first?.text,
+               let userNumber = Int(safeText)
+            else {
+               return
+            }
             let solveNumber = Int.random(in: 1...3)
             if userNumber == solveNumber {
-                self.labelResultGuessNumber.text = "Угадал! Молодечик! 😜"
+                self.resultGuessNumberLabel.text = "Угадал! Молодечик! 😜"
             } else {
-                self.labelResultGuessNumber.text = "Не угадал... 😢"
+                self.resultGuessNumberLabel.text = "Не угадал... 😢"
             }
             print(solveNumber)
         }
         alertController.addTextField(configurationHandler: nil)
-        alertController.addAction(action)
-        self.present(alertController, animated: true, completion: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
     
-    @objc func actionButtonSum() {
+    @objc func buttonSumAction() {
         let alertController = UIAlertController(
             title: "Сумма",
             message: "Введите число 1 и число 2, каждое в отдельное текстовое поле",
             preferredStyle: .alert
         )
-        let action = UIAlertAction(title: "ОК", style: .default) { _ in
-            guard let safeTextFieldOne = alertController.textFields?.first?.text,
-                    let safeTextFieldTwo = alertController.textFields?[1].text else { return }
-            guard let numberOne = Int(safeTextFieldOne), let numberTwo = Int(safeTextFieldTwo) else { return }
-            self.labelSum.text = String(numberOne + numberTwo)
+        let alertAction = UIAlertAction(title: "ОК", style: .default) { _ in
+            guard
+                alertController.textFields?.count == 2,
+                let safeTextFieldOne = alertController.textFields?.first?.text,
+                let safeTextFieldTwo = alertController.textFields?[1].text,
+                let numberOne = Int(safeTextFieldOne),
+                let numberTwo = Int(safeTextFieldTwo)
+            else {
+               return
+            }
+            self.sumLabel.text = ("\(numberOne+numberTwo)")
         }
         alertController.addTextField(configurationHandler: nil)
         alertController.addTextField(configurationHandler: nil)
-        alertController.addAction(action)
-        self.present(alertController, animated: true, completion: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func setUI() {
+        view.addSubview(fioLabel)
+        view.addSubview(resultGuessNumberLabel)
+        view.addSubview(guessNumberButton)
+        view.addSubview(sumLabel)
+        view.addSubview(sumButton)
+    }
+    
+    private func createStartAlert() {
+        let alertController = UIAlertController(title: "ФИО", message: "Введите ФИО", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "ОК", style: .default) { _ in
+            if let safeText = alertController.textFields?.first?.text {
+                self.fioLabel.text = safeText
+            }
+        }
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
