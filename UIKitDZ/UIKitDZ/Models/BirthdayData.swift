7421e5052ef523205c6603159d2b15b9c784f8f4
Temich93s <nil>
2022-09-25 20:50:33 +1000

    сделал урок 5

---
+++ UIKitDZ/UIKitDZ/Models/BirthdayData.swift
@@ -0,0 +1,18 @@
+//
+//  BirthdayData.swift
+//  UIKitDZ
+//
+//  Created by 2lup on 25.09.2022.
+//
+
+import Foundation
+
+/// BirthdayData: Данные о человеке и его дне рождении
+/// - name: Имя человека
+/// - countDays: Количество оставшихся дней до дня рождения
+/// - description: Подробное описание оставшихся дней до дня рождения
+struct BirthdayData {
+    var name = ""
+    var description = ""
+    var countDays = ""
+}
